47ecdab68348fa77f2d3b91da2d2e21eee8fec1b
sromero1905 <santirom1905@gmail.com>
2024-09-20 23:38:47 -0300

    se agrega design estilizado v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
-    extend: {
-      fontFamily: {
-        montserrat: ['"Montserrat"', 'sans-serif'],
-      },
+    screens: {
+      mob: "375px",
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1280px",
+      laptopl: "1440px",
     },
+    extend: {},
   },
   plugins: [],
 };
