13dfbb052f0476cb3c56976d28d137df92134546
sromero1905 <santirom1905@gmail.com>
2024-10-08 22:05:37 -0300

    se finaliza etapa piloto proyecto

--- src/Components/ProjectDetail.tsx
+++ src/Components/ProjectDetail.tsx
@@ -19,7 +19,10 @@ const ProjectDetail = () => {
     return (
       <div className="min-h-screen flex flex-col justify-center items-center bg-black text-white">
         <h2 className="text-4xl font-bold mb-6">Proyecto no encontrado</h2>
-        <Link to="/" className="text-gray-400 hover:text-gray-300 underline inline-flex items-center">
+        <Link
+          to="/"
+          className="text-gray-400 hover:text-gray-300 underline inline-flex items-center"
+        >
           <FiArrowLeft className="inline mr-2" />
           Volver a la lista de proyectos
         </Link>
@@ -32,7 +35,7 @@ const ProjectDetail = () => {
       className="min-h-screen bg-black flex flex-col items-center py-20 px-4 lg:px-8"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 0.8, ease: "easeInOut" }} // Transición de carga
+      transition={{ duration: 1, ease: "easeInOut" }}
     >
       {/* Navbar flotante */}
       <FloatingNav
@@ -44,23 +47,22 @@ const ProjectDetail = () => {
       />
 
       {/* Contenedor principal */}
-      <div className="relative w-full max-w-5xl mb-12"> {/* Aumentar max-w para mayor ancho */}
-        {/* Imagen principal - Ajustes para estar menos larga y más ancha */}
+      <div className="relative w-full max-w-5xl mb-12">
+        {/* Imagen principal */}
         <motion.img
           src={project.image}
           alt={project.title}
-          className="w-full h-[600px] object-cover rounded-lg shadow-lg transition-transform duration-300 hover:scale-105" // Aumentar altura a 600px
-          initial={{ opacity: 0, y: 20 }} // Entrada más suave
-          animate={{ opacity: 1, y: 0 }} 
-          transition={{ duration: 1.2, ease: "easeInOut" }} // Transición más suave
-          style={{ marginTop: '-20px' }} // Ajuste para que la imagen esté más arriba
+          className="w-full h-[600px] object-cover rounded-lg shadow-lg"
+          initial={{ opacity: 0, y: 10 }} // Movimiento más sutil
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
         />
-        {/* Título sobre la imagen - Mejorado */}
+        {/* Título sobre la imagen */}
         <motion.div
           className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent p-4 rounded-b-lg"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.4, ease: "easeInOut" }} // Transición más tranquila
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.7, ease: "easeInOut" }}
         >
           <h1 className="text-4xl font-bold text-white">{project.title}</h1>
         </motion.div>
@@ -70,10 +72,10 @@ const ProjectDetail = () => {
       <div className="w-full max-w-3xl space-y-8 text-gray-300">
         <motion.div
           className="leading-relaxed"
-          initial={{ opacity: 0, x: -20 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1.2, delay: 0.5, ease: "easeInOut" }} // Transición más suave
-          viewport={{ once: false }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          viewport={{ once: true }}
         >
           <h2 className="text-3xl font-semibold text-white mb-2">Descripción</h2>
           <p className="text-lg">{project.details.detail1}</p>
@@ -81,47 +83,67 @@ const ProjectDetail = () => {
 
         <motion.div
           className="leading-relaxed"
-          initial={{ opacity: 0, x: 20 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1.2, delay: 0.7, ease: "easeInOut" }} // Transición más suave
-          viewport={{ once: false }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          viewport={{ once: true }}
         >
           <h2 className="text-3xl font-semibold text-white mb-2">Aspectos Clave</h2>
           <p className="text-lg">{project.details.detail2}</p>
         </motion.div>
+
+        {/* Nueva sección para detail3 */}
+        <motion.div
+          className="leading-relaxed"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          viewport={{ once: true }}
+        >
+          <h2 className="text-3xl font-semibold text-white mb-2">Conclusiones</h2>
+          <p className="text-lg">{project.details.detail3}</p>
+        </motion.div>
       </div>
 
+      {/* Imagen adicional */}
+      {project.additionalImage && (
+        <motion.div
+          className="w-full max-w-5xl mt-12 mb-8"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1.2, ease: "easeInOut" }}
+          viewport={{ once: true }}
+        >
+          <img
+            src={project.additionalImage}
+            alt="Imagen adicional"
+            className="w-full h-[500px] object-cover rounded-lg shadow-lg"
+          />
+        </motion.div>
+      )}
+
       {/* Botón para visitar la página del proyecto */}
-      <motion.div
-        className="mt-12"
-        initial={{ opacity: 0, scale: 0.9 }}
-        whileInView={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1.2, delay: 0.9, ease: "easeInOut" }} // Transición más suave
-        viewport={{ once: false }}
-      >
+      <div className="mt-8">
         <a
           href={project.link}
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex items-center bg-gray-700 hover:bg-gray-600 text-white font-medium py-3 px-6 rounded-full transition-all duration-300"
+          className="inline-flex items-center bg-gray-700 hover:bg-gray-600 text-white font-medium py-4 px-8 rounded-full transition-all duration-300"
         >
           Visitar página del proyecto
         </a>
-      </motion.div>
+      </div>
 
       {/* Enlace para volver a la lista de proyectos */}
-      <motion.div
-        className="mt-8"
-        initial={{ opacity: 0, y: 10 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1.2, delay: 1, ease: "easeInOut" }} // Transición más suave
-        viewport={{ once: false }}
-      >
-        <Link to="/" className="text-gray-400 inline-flex items-center hover:text-gray-300 transition-colors">
+      <div className="mt-8">
+        <Link
+          to="/"
+          className="text-gray-400 inline-flex items-center hover:text-gray-300 transition-colors"
+        >
           <FiArrowLeft className="mr-2" />
           Volver a la lista de proyectos
         </Link>
-      </motion.div>
+      </div>
     </motion.div>
   );
 };
